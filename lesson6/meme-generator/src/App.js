03a34ba4c99edf31657900dff151c3bebd9d83dc
Emre Ulgac <ulgacemre@gmail.com>
2023-05-30 21:31:01 +0200

    React js - course content

---
+++ lesson6/meme-generator/src/App.js
@@ -0,0 +1,38 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import Meme from './components/Meme';
+import Counter from './components/Counter';
+import Form from './components/Form';
+import Signup from './components/Signup';
+import Ternary from './components/Ternary';
+import UseEffect from './components/UseEffect';
+import MUI from './components/MUI';
+import Input from './components/Input';
+
+
+function App() {
+  return (
+    <div className="App">
+      {/**  
+        * 
+           <Counter />
+           <Form />
+           <Signup />
+           <Ternary />
+           <UseEffect />
+ 
+      
+      <MUI />
+       <Input type="password" label="Password label only"/>
+        */}
+
+      <Navbar />
+      <main className='main'>
+        <Meme />
+      </main>
+
+    </div>
+  );
+}
+
+export default App;
